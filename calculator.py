27ecd5dfcfacd79c796123a81a9acf2816f37e74
cod <rudgh758525@gmail.comlsgit config user.email rudgh758525@gmail.com>
2024-10-29 13:38:21 +0900

    :

--- calculator.py
+++ calculator.py
@@ -5,17 +5,5 @@ def add(a, b):
 def subtract(a, b):
   return a-b
 
-<<<<<<< HEAD
-  def divide(a, b):
-    return a/b
-=======
-def multuplay(a, b):
-  return a*b
-
-<<<<<<< HEAD
-def divide(a, b):
+def divide_new(a,b):
   return a/b
->>>>>>> main
-=======
-
->>>>>>> main
